a85d200b85b8377083fc6765ca77ff491d53eab6
Mehul Bawadia <mehulbawadia@gmail.com>
2021-04-27 15:25:28 +0530

    Login and logout of user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,9 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    /**
+     * Login a existing user.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|email:filter|exists:users,email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+        if (! $user || ! Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Invalid credentials.',
+            ], 401);
+        }
+
+        $token = $user->createToken('my-app-token')->plainTextToken;
+
+        return response([
+            'message' => 'User logged in successfully.',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
     /**
      * Register a new user.
      *
@@ -34,5 +64,20 @@ class AuthController extends Controller
             'user' => $user,
             'token' => $token,
         ], 201);
+    }
+
+    /**
+     * Logging out the user.
+     *
+     * @param  \Illuminate\Http\Request $request [description]
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        return response([
+            'message' => 'Logged out.'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\ProductsController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/user/login', [AuthController::class, 'login']);
 Route::post('/user/register', [AuthController::class, 'register']);
 
 Route::get('/products/{id}', [ProductsController::class, 'show']);
@@ -22,6 +23,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/products/create', [ProductsController::class, 'store']);
     Route::patch('/products/{id}', [ProductsController::class, 'update']);
     Route::delete('/products/{id}', [ProductsController::class, 'destroy']);
+
+    Route::post('/user/logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
